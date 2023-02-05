d2a939a51d33e715c0cd03a84dd8cb7da6f96f7a
kedinha <kedmyknow@gmail.com>
2023-02-05 01:17:28 -0300

    Update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        '*.html',
+        './src/*.{html,js}',
+        './dist/*.{js}',
+        './assets/**/*.{css,js}',
+        './examples/*.{html}',
+    ],
+    theme: {
+        fontFamily: {
+            lato: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
+            sans: ['ui-sans-serif', 'system-ui'],
+            display: ['Oswald'],
+            body: ['"Lato"'],
+        },
+        container: {
+            center: true,
+        },
+        extend: {
+            padding: '2rem'[('hover', 'focus')],
+        },
+        borderWidth: {
+            DEFAULT: '1px',
+            0: '0',
+            2: '2px',
+            3: '3px',
+            4: '4px',
+            6: '6px',
+            8: '8px',
+        },
+    },
+    plugins: [],
 }
